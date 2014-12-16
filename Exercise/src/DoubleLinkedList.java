9e250c5a07a0f7b75acb18ef7f18aae8044da1b3
Artem Shoobovych <shybovycha@gmail.com>
2014-12-16 16:55:32 +0100

    Added many methods

--- Exercise/src/DoubleLinkedList.java
+++ Exercise/src/DoubleLinkedList.java
@@ -1,7 +1,53 @@
 import java.util.Iterator;
 
 public class DoubleLinkedList<T> implements Iterable {
-    public DoubleLinkedList() {}
+    protected Element<T> head;
+    protected Element<T> tail;
+
+    public DoubleLinkedList() {
+
+    }
+
+    public DoubleLinkedList(Iterable elements) {
+
+    }
+
+    public Element<T> getHead() {
+        return head;
+    }
+
+    public Element<T> getTail() {
+        return tail;
+    }
+
+    // insertAtEnd
+    public void add(T val) {
+        Element<T> elt = new Element<T>(val);
+        elt.previous = this.tail;
+        this.tail = elt;
+    }
+
+    // insertAt
+    public void add(int index, T val) {
+        Element<T> curr = this.head;
+
+        int i = 1;
+
+        while (curr != null && i < index) {
+            curr = curr.next;
+            i++;
+        }
+
+        if (curr == null) {
+            return;
+        }
+
+        Element<T> elt = new Element<T>(val);
+
+        elt.previous = curr;
+        elt.next = curr.next;
+        curr.next = elt;
+    }
 
     @Override
     public Iterator iterator() {
--- Exercise/src/Element.java
+++ Exercise/src/Element.java
@@ -1,3 +1,41 @@
 public class Element<T> {
-    public Element() {}
+    protected T value;
+    protected Element<T> previous;
+    protected Element<T> next;
+
+    public Element() {
+        this.value = null;
+        this.next = null;
+        this.previous = null;
+    }
+
+    public Element(T value) {
+        this.value = value;
+        this.next = null;
+        this.previous = null;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public Element<T> getPrevious() {
+        return previous;
+    }
+
+    public Element<T> getNext() {
+        return next;
+    }
+
+    public void setPrevious(Element<T> previous) {
+        this.previous = previous;
+    }
+
+    public void setNext(Element<T> next) {
+        this.next = next;
+    }
 }
